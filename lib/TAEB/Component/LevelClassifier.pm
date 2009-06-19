43383c273f268bf720dd624b7b999eaa2b539fa3
Stefan O'Rear <stefanor@cox.net>
2009-06-18 19:33:05 -0700

    Discard the notion of nuanced truth
    This allows a considerable amount of simplification; in particular
    the constraint solver is no longer needed.

--- lib/TAEB/Component/LevelClassifier.pm
+++ lib/TAEB/Component/LevelClassifier.pm
@@ -1,20 +1,16 @@
 # This is the new (Jun09) level classifier.
 #
-# First step: scan levels for intrinsic properties.  We assign for each
-# (level,tag) pair a rating:
-#    definitely not: NO
-#    probably not:   not without deliberate bonesing
-#    likely not:     not likely, beware bones (but not neccessarily so)
-#    ''              neutral
-#    likely..definitely: analogout
+# First step: scan levels for intrinsic properties.  Properties are
+# named with short strings like 'oracle'; each recognizer will return
+# yes, no, or unknown for a given level,tag pair.  This is only done
+# when the levels change.
 #
-# This is only done when the levels change.
+# Second step is a process of local inference; given levels that always
+# or never have other levels next to them, we assign that property to
+# the corresponding level.
 #
-# Second step is to run an iterative constraint solver over the dataset.
-# Our termination condition is that any given rating can only become
-# more definite, or flip to negative at the same definity; this defines
-# a well-ordering over our state space, and by Kleene's theorem a fixed
-# point will be reached.
+# Finally a uniqueness fixup is done, performing deductions by elimination
+# on the levels.
 
 package TAEB::Component::LevelClassifier;
 
@@ -25,7 +21,7 @@ use TAEB::OO;
 
 has _intrinsic_tags => (
     is      => 'ro',
-    isa     => 'HashRef[HashRef[Int]]',
+    isa     => 'HashRef[HashRef[Maybe[Bool]]]',
     default => sub { {} },
 );
 
@@ -33,26 +29,23 @@ has _intrinsic_tags => (
 
 has _dungeon_tags => (
     is      => 'ro',
-    isa     => 'HashRef[HashRef[Int]]',
+    isa     => 'HashRef[HashRef[Maybe[Bool]]]',
     default => sub { {} },
 );
 
-# Set to 1 whenever the iteration changes something.
+# For unique things and exclusive types, store the holders here.
 
-has _reiterate_solver => (
-    isa  => 'Bool',
-    is   => 'rw',
+has _candidates => (
+    is      => 'ro',
+    isa     => 'HashRef[Maybe[String]]',
+    default => sub { {} },
 );
 
-use constant {
-    DEFINITELY_NOT => -3,
-    PROBABLY_NOT   => -2,
-    LIKELY_NOT     => -1,
-    NEUTRAL        => 0,
-    LIKELY         => 1,
-    PROBABLY       => 2,
-    DEFINITELY     => 3,
-};
+has _types => (
+    is      => 'ro',
+    isa     => 'HashRef[Maybe[String]]',
+    default => sub { {} },
+);
 
 use constant special_tags => [qw/
     minetown minesend
@@ -68,151 +61,65 @@ use constant special_tags => [qw/
 use constant unique_tags => [ @{ special_tags }, qw/
     minefork sokofork questfork ludiosfork vladfork vibratingsquare/ ];
 
+my %_unique_tag = map { $_ => 1 } unique_tags;
+
 #use constant branch_tags => [qw/mines sokoban quest dungeon gehennom ludios
 #        planes vlad/],
 
 use constant level_type_tags => [ @{ special_tags },
     qw/minefill dgnfill mazefill questfill1 questfill2/ ];
 
+my %_level_type_tag = map { $_ => 1 } level_type_tags;
+
 ################
 
-sub _rate {
-    my ($self, $level, $tag, $new) = @_;
-    my $ref = \ (($self->_dungeon_tags->{$level} ||= {})->{$tag} ||= 0);
+# Called by local_to_global to mark a property as true or false
+sub _insert_one {
+    my ($self, $level, $tag, $bool) = @_;
 
-    return $$ref unless defined $new;
-
-    if (abs($new) < $$ref || ($new > 0 && $$ref == -$new)) {
-        die "Termination order for solver has been violated.  Attempting to rerate ($level,$tag) from $$ref to $new.";
+    if ($bool && $_unique_tag{$tag} &&
+            ($self->_candidates->{$tag} ||= $level) ne $level) {
+        warn "$level and " . $self->_candidates->{$tag} . " are both $tag??";
+        $bool = 0;
     }
 
-    if ($$ref != $new) {
-        $self->_reiterate_solver(1);
+    if ($bool && $_level_type_tag{$tag} &&
+            ($self->_types->{$level} ||= $tag) ne $tag) {
+        warn "$level is both $tag and " . $self->_types->{$level} . "??";
+        $bool = 0;
     }
 
-    $$ref = $new;
+    $self->_dungeon_tags->{$level}{$tag} = $bool;
 }
 
-sub _solver {
+sub _local_to_global {
     my ($self) = @_;
 
-    my @levels = keys %{ $self->_intrinsic_tags };
-
-    for my $level (@levels) {
-        $self->_dungeon_tags{$level} = { %{ $self->_intrinsic_tags->{$level} } };
-    }
-
-    $self->_reiterate_solver(1);
-
-    while($self->_reiterate_solver) {
-        $self->_reiterate_solver(0);
-
-        # A level cannot be Oracle and Minetown, or dungeons and mines
-        for my $level (@levels) {
-#            $self->_constrain_disjunction($level, $self->branch_tags);
-            $self->_constrain_disjunction($level, $self->level_type_tags);
-        }
-
-        # Two levels cannot be the Oracle
-        for my $tag ($self->unique_tags) {
-            $self->_constrain_disjunction(\@levels, $tag);
-        }
-
-        # Topographical connection rules go here?
-    }
-}
-
-sub _get_candidate {
-    my ($self, $level_set, $tag_set) = @_;
-
-    my $candidate_rating = NEUTRAL;
-    my ($candidate_level, $candidate_tag, @ties);
-
-    for my $level (@$level_set) {
-        for my $tag (@$tag_set) {
-
-            my $rating = $self->_rate($level, $tag);
-            next unless $rating > NEUTRAL;
-
-            if ($rating == $candidate_rating) {
-                push @ties, "$level,$tag";
-            } else {
-                $candidate_rating = $rating;
-                $candidate_level = $level;
-                $candidate_tag = $tag;
-                @ties = "$level,$tag";
-            }
-        }
-    }
-
-    if ($candidate_rating == DEFINITELY && @ties > 1) {
-        my $tielist = join ", ", map { "($_)" } @ties;
-
-        die "All of $tielist were rated at definitely.  This indicates a bug in the recognizers."
-    }
-
-    return ($candidate_rating, $candidate_level, $candidate_tag, @ties);
-}
-
-# One definitely makes everything else d.not, etc
-sub _constrain_disjunction {
-    my ($self, $lset, $tset) = @_;
-
-    $lset = [$lset] unless ref $lset;
-    $tset = [$tset] unless ref $tset;
-
-    my $candidate_rating = NEUTRAL;
-    my ($candidate_rating, $candidate_level, $candidate_tag, @ties) =
-        $self->_get_candidate($lset, $tset);
-
-    for my $level (@$lset) {
-        for my $tag (@$tset) {
-
-            next if $level eq $candidate_level && $tag eq $candidate_tag;
-
-            next if $self->_rate($level,$tag) < -$candidate_rating;
-
-            $self->_rate($level,$tag, -$candidate_rating);
+    while (my ($level, $info) = each %{ $self->_intrinsic_tags }) {
+        while (my ($tag, $value) = each %$info) {
+            $self->_insert_one($level, $tag, $value);
         }
     }
 }
 
 ##########
 
-sub rate {
-    my $self = shift;
-    string_ratings->[$self->_rate(@_)];
+sub is {
+    my ($self, $level, $tag) = shift;
+    $self->_dungeon_tags->{$level->stable_name}{$tag};
 }
 
-#XXX these are inefficient
 sub candidate {
     my ($self, $tag) = @_;
 
-    my ($rating, $name, $tag, @ties) = 
-        $self->_get_candidate([ keys %{ $self->_dungeon_tags } ], [ $tag ]);
-
-    return $self->stable2level($name);
+    $self->_candidates->{$tag};
 }
 
 sub classify {
     my ($self, $level) = @_;
 
-    my ($rating, $name, $tag, @ties) = 
-        $self->_get_candidate([ $self->level2stable($level) ],
-            [ $self->level_type_tags ]);
-
-    return $tag;
+    $self->_types->{$level->stable_name};
 }
-
-#sub branch {
-#    my ($self, $level) = @_;
-#
-#    my ($rating, $name, $tag, @ties) = 
-#        $self->_get_candidate([ $self->level2stable($level) ],
-#            [ $self->branch_tags ]);
-#
-#    return $tag;
-#}
 
 __PACKAGE__->meta->make_immutable;
 
